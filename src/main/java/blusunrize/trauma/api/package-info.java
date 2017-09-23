0266bd192a5c3e2103c8643c57463ad49fc15ac5
BluSunrize <danny.rad10@yahoo.de>
2017-09-23 09:50:55 +0200

    Added package-info for API

---
+++ src/main/java/blusunrize/trauma/api/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * @author BluSunrize
+ * @since 23.09.2017
+ */
+@API(owner = "Trauma", apiVersion = "1.0", provides = "Trauma|API")
+package blusunrize.trauma.api;
+
+import net.minecraftforge.fml.common.API;
